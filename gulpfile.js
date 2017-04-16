47c0de4a137c4e2345db7e6bcbcac94698c0f1b4
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-04-16 13:21:51 +0100

    Arreglado gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ return gulp.src('./conversor/**/*')
 });
 
 gulp.task('deploypush', ['deploy'], function() {
-  return gulp.src('./conversor/*')
+  return gulp.src('./conversor/**/*')
   .pipe(ghPages(['git@github.com:ULL-ESIT-DSI-1617/eliminando-switch-smell-andrea-carlos-35l1-p5.git']))
 })
     
