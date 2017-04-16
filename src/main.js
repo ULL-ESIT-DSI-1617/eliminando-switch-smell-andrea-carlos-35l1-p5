80237176dcbca83526f911a36deda7962395a3cd
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-04-16 11:20:18 +0000

    Update 2017-04-16T11:20:16.565Z

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let myMedida = {};
+let magnitud = {};
+
+function main(){
+
+    let calcular = new Medida(original.value)
+
+    converted.innerHTML = calcular.convertir(magnitud.calculates, myMedida.magnitudes);
+    
+}
